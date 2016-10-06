0956890bff04a74faf1e14ad31fe6e5e90afd7b0
verwec <sebastian@vertragswecker.de>
2016-10-06 10:07:28 +0200

    Add some more challenges

--- lib/ruby_trainer/challenge_builder.rb
+++ lib/ruby_trainer/challenge_builder.rb
@@ -4,9 +4,38 @@ module RubyTrainer
       challenges = []
 
       challenges << RubyTrainer::Challenge.build do
-        help 'Enter 1 + 1'
-        success_message 'Das hast du gut gemacht'
-        test { |result| result == 2 }
+        help 'For your first bit of Ruby, try typing some math, like: 2+6'
+        success_message 'Success!'
+        error_message 'Try again'
+        test { |res| res == 8 }
+      end
+
+      challenges << RubyTrainer::Challenge.build do
+        help 'Type your first name in quotes, like this: "Jimmy"'
+        success_message 'Success'
+        error_message 'Try again'
+        test { |res| res.is_a?(String) && res.length > 0 }
+      end
+
+      challenges << RubyTrainer::Challenge.build do
+        help 'Type: "Jimmy".reverse", using your own name where the string goes.'
+        success_message 'Success'
+        error_message 'Try again'
+        test { |res| res.is_a?(String) && res.length > 0 }
+      end
+
+      challenges << RubyTrainer::Challenge.build do
+        help 'Try typing "Jimmy".length using your name'
+        success_message 'Success'
+        error_message 'Try again'
+        test { |res| res.is_a?(Fixnum) && res > 0 }
+      end
+
+      challenges << RubyTrainer::Challenge.build do
+        help 'Let\'s get crazy now, and multiply your name by 5. Follow the following format: "Jimmy" * 5'
+        success_message 'Success'
+        error_message 'Try again'
+        test { |res| res.is_a?(String) && res.length > 0 }
       end
 
       challenges
